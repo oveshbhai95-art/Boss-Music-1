9523dcfd6a7c4a454d1d22343ee37c1614eb9084
ovesh boss <oveshboss.08@gmail.com>
2026-01-11 12:15:51 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -6,25 +6,13 @@
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 from typing import Union, List
-
 from pyrogram import filters
-
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
-other_filters2 = filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
-
+# Humne yahan se ~filters.edited hata diya hai taaki crash na ho
+other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
+other_filters2 = filters.private & ~filters.via_bot & ~filters.forwarded
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
-
