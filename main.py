71d04f083d43e7aa3e65099478cad45af9f53091
ovesh boss <oveshboss.08@gmail.com>
2026-01-12 18:19:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from pyrogram import Client as Bot, filters # filters yahan add kiya hai
+from pyrogram import Client as Bot, filters
 from callsmusic import run
 from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 
@@ -7,6 +7,9 @@ from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 if not hasattr(filters, "edited"):
     filters.edited = filters.create(lambda _, __, ___: False)
 # --------------------------------------------------
+
+# Aapka String Session yahan define kar diya hai
+STRING_SESSION = "BQJBLZQAOy4ydKZNdb336ahf4V0P86NODeLnIq_oeGJdrBkkyQtxJZqMs_dfL2G182Q5fdt4umF-WOCqke0HVzBIb9igvqdjhKlQci5FnpS8kPIazGOSGIVYlULAttLyvFp0_fji4Xv43fC6HiKAonspmBLGQo1wQGOxkv-K8vGsbpPyEbnGjKTIUrzVyqmR5IuDFSLULLP4d0c5wsE4xL6seY9YxLTO88cbWbRV0mDRILhdJ8m6ZXve3dgS70HwxEYe9btcbYVDYFnhU_fz7dkz4M-VPivvfxIBwQRoWVeKVZTpOz0sCK5tzA3KducVy6GU6Zt3WTaU3-cDR7VUiDGa7d6iwgAAAAH_5sbSAA"
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
@@ -22,5 +25,8 @@ bot = Bot(
 
 print("[INFO]: CYBERMUSIC STARTED!")
 
-bot.start()
-run()
+# Bot start karne ka sahi tarika
+if __name__ == "__main__":
+    bot.start()
+    # Yahan humne callsmusic ko run kiya hai jo aapke assistant ko handle karega
+    run()
